c07d4a6d30f7221083d90a74bb564018cb6c8f1e
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-16 16:58:25 +0600

    add to project second ViewConttroller and title for ViewController

---
+++ NavApp/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  NavApp
+//
+//  Created by Shakir Kadirov on 16.01.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Настройки"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
